faf37e5b3d622ee13eb75fb70e43f521f8fea6c8
Ziad Khaled <ziadkh99@hotmail.com>
2024-11-23 21:46:49 +0200

    Solve all Ch9_Dictionaries excercises

--- Excercises/PythonForEverybody/test.py
+++ Excercises/PythonForEverybody/test.py
@@ -1,2 +1,22 @@
+file_name = input('Enter file name: ')
+
+try:
+    file_handle = open(file_name)
+except:
+    print(f'Cannot open file: {file_name}')
+    exit()
+
+
+emails_dict = dict()
+
+for line in file_handle:
+    if line.startswith('From '):
+        email = line.split()[1]
+        email = email.split('@')[1]
+        emails_dict[email] = emails_dict.get(email, 0) + 1
+
+
+print(emails_dict)
+
 
 
