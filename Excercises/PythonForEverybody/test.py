4f7d81d1cbbd7dd5e8c0eff3055ad158db06ae40
Ziad Khaled <ziadkh99@hotmail.com>
2024-11-27 23:42:13 +0200

    Complete Ch12: Networked Programs

--- Excercises/PythonForEverybody/test.py
+++ Excercises/PythonForEverybody/test.py
@@ -1,22 +1,10 @@
-file_name = input('Enter file name: ')
-
-try:
-    file_handle = open(file_name)
-except:
-    print(f'Cannot open file: {file_name}')
-    exit()
+import urllib.request 
 
 
-emails_dict = dict()
+url = input('URL: ')
+
+file_handle = urllib.request.urlopen(url)
 
 for line in file_handle:
-    if line.startswith('From '):
-        email = line.split()[1]
-        email = email.split('@')[1]
-        emails_dict[email] = emails_dict.get(email, 0) + 1
-
-
-print(emails_dict)
-
-
+    print(line.decode().strip())
 
