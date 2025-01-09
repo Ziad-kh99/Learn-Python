ab25f37abd5a5008023d5ffce6c1253645a59dd2
Ziad Khaled <ziadkh99@hotmail.com>
2025-01-09 18:05:41 +0200

    Solve some string excercises

---
+++ Excercises/RealPython/Beginner/StringConcatemation.py
@@ -0,0 +1,8 @@
+# Create two strings, concatenate them, and print the resulting string. 
+# And then there’s the question: can you also add a space between them only using concatenation?
+
+s1 = 'First sentance'
+s2 = 'second sentance'
+s3 = s1 + ' ' + s2
+
+print(s3)
---
+++ Excercises/RealPython/Beginner/WhatIsAString.py
@@ -0,0 +1,18 @@
+# double quotes in the string:
+print('My name is "Ziad".')
+print("My name is \"Ziad\".")
+
+# apostroph in the string:
+print("This is Sarah's ring.")
+print('This is Sarah\'s ring.')
+
+# multi line string:
+print('''First Line
+Second Line
+    sub-second Line
+Third Line
+''')
+
+# write a stting in multi-line but print it as a single line:
+print('This is text printed as a one-line \
+but it written in multi-line in the code.')
