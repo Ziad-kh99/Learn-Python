06ee9e938faf3a7b138c6b2e6e85adf7e95200ae
Ziad Khaled <ziadkh99@hotmail.com>
2025-01-10 00:09:54 +0200

    Learn about Modules and Packages

---
+++ Topics/ModulesAndPackages/calc_package/calculator.py
@@ -0,0 +1,8 @@
+def add(n1, n2):
+    '''Return the addition of two numbers'''
+    return n1 + n2
+
+def substract(n1, n2):
+    '''Return the substraction of two numbers'''
+    return n1 - n2 
+
---
+++ Topics/ModulesAndPackages/main.py
@@ -0,0 +1,9 @@
+from calc_package.calculator import add
+from calc_package import calculator
+
+
+addition = add(5, 4)
+substraction = calculator.substract(8, 3)
+
+print(substraction)
+
