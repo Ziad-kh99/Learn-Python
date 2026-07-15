2203f0085a7b2379522e8525322e66fc6449b4db
Ziad Khaled <ziad.dev99@gmail.com>
2026-07-15 21:29:40 +0300

    Reveiw some python basics

---
+++ Topics/Basics/index.py
@@ -0,0 +1,9 @@
+x = int(input('first number: '))
+y = int(input('second number: '))
+
+if x < y:
+    print('x is less than y')
+elif x > y:
+    print('x is greater than y')
+else:
+    print('x is equal to y')
